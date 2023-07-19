e24913a75e4499b30f7dc96eda4a83942925c2fa
Fred Burgess <fredburgess@Freds-MacBook-Air-2.local>
2023-07-19 11:58:43 +0100

    add update dependencies

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify
 import script
 import threading
+import logging
 
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+
 
 def run_script():
     script.main()
@@ -13,7 +16,9 @@ def trigger_script():
     # Call your script here
     thread = threading.Thread(target=run_script)
     thread.start()
-    return jsonify({'message': 'Script is being run in another thread'}), 200
+    message = 'Script is being run in another thread'
+    app.logger.info(message)
+    return jsonify({'message': message}), 200
 
 if __name__ == '__main__':
     app.run()
