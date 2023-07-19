708e92f288c656c50d141c72567e970bcbd30f6a
Fred Burgess <fredburgess@Freds-MacBook-Air-2.local>
2023-07-19 14:21:56 +0100

    add update dependencies

--- script.py
+++ script.py
@@ -19,7 +19,7 @@ def main():
     latest_record = records[0]['fields']
 
     # construct the prompt
-    prompt = f"This description is about my {latest_record['Relation']}: they grew up in {latest_record['Location']}. They describe themselves in 3 words as {latest_record['Three words']}. Their main interests are {latest_record['What are their main interests?']}. A typical meeting between us looks like {latest_record['Typical meeting']}. And I would describe the conversation as {latest_record['Conversation']}. \n\nFinally, they like {latest_record['Fiction/Non-fiction']} books. \n\nRecommend me 5 books based on this description. Make sure the recommendations aren't really obvious. "
+    prompt = f"This description is about my {latest_record['Relation']}: they grew up in {latest_record['Location']}. They describe themselves in 3 words as {latest_record['Three words']}. Their main interests are {latest_record['What are their main interests?']}. A typical meeting between us looks like {latest_record['Typical meeting']}. And I would describe the conversation as {latest_record['Conversation']}. \n\nFinally, they like {latest_record['Fiction/Non-fiction']} books. \n\nRecommend me 5 books based on this description. Make sure the recommendations aren't really obvious. At the end of the response, provide a list of ISBN numbers for the books."
 
     # Set your OpenAI key
     openai.api_key = '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'
