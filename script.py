5b980ecfa277cdef1ee6d2c8019297d9cc64b02d
Fred Burgess <fredburgess@Freds-MacBook-Air-2.local>
2023-07-19 16:22:56 +0100

    add update dependencies

--- script.py
+++ script.py
@@ -19,7 +19,7 @@ def main():
     latest_record = records[0]['fields']
 
     # construct the prompt
-    prompt = f"This description is about my {latest_record['Relationship']}: they grew up in {latest_record['Location']}. They describe themselves in 3 words as {latest_record['Three words']}. Their main interests are {latest_record['What are their main interests?']}. A typical meeting between us looks like {latest_record['Typical meeting']}. And I would describe the conversation as {latest_record['Conversation']}. \n\nFinally, they like {latest_record['Fiction/Non-fiction']} books. \n\nRecommend me 5 books based on this description. Make sure the recommendations aren't really obvious. At the end of the response, provide a python dictionary of ISBN numbers for the books."
+    prompt = f"This description is about my {latest_record['Relationship']}: they grew up in {latest_record['Location']}. They describe themselves in 3 words as {latest_record['Three words']}. Their main interests are {latest_record['What are their main interests?']}. A typical meeting between us looks like {latest_record['Typical meeting']}. And I would describe the conversation as {latest_record['Conversation']}. \n\nFinally, they like {latest_record['Fiction/Non-fiction']} books. \n\nRecommend me 5 books based on this description. Make sure the recommendations aren't really obvious. Provide the response as a Python dictionary with key value pairs being Book title and Explanation respectively."
 
     # Set your OpenAI key
     openai.api_key = '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'
@@ -33,6 +33,7 @@ def main():
       ]
     )
     response_text = response.choices[0].message.content.strip()
+    print(response_text)
     books = re.findall(r'\n\d\..*? \((.*?)\)', response_text)
     print(books)
     return books
