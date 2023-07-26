9bac439eac5a8754a9c20d5ddec28c9fedf24562
Fred Burgess <fredburgess@Freds-MacBook-Air-2.local>
2023-07-26 16:43:46 +0100

    add

--- script.py
+++ script.py
@@ -46,7 +46,7 @@ def main():
     recommendations_table = Airtable(base_key, 'Recommendations', api_key)
 
     # Insert books into 'Recommendations' table
-    recommendations_table.insert({'Books': books})
+    recommendations_table.insert(books)
 
     return books
 
