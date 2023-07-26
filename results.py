c5c9d895478904db51c900642190e2ae8e340bec
Fred Burgess <fredburgess@Freds-MacBook-Air-2.local>
2023-07-26 16:31:54 +0100

    add

--- results.py
+++ results.py
@@ -20,7 +20,7 @@ def results():
     latest_record = records[0]['fields']
     print(latest_record)
     booklist_str = next(iter(latest_record.values()))
-    booklist_dict = json.loads(booklist_str)
+    #booklist_dict = json.loads(booklist_str)
     print(booklist_dict)
     return booklist_dict
 
