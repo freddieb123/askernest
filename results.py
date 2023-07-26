f9bbaccb464be43ef9b95a23aa287100f4ff5709
Fred Burgess <fredburgess@Freds-MacBook-Air-2.local>
2023-07-26 15:53:09 +0100

    add

--- results.py
+++ results.py
@@ -18,8 +18,8 @@ def results():
     records = airtable.get_all(maxRecords=1, sort=[('created_time', 'desc')])
     latest_record = records[0]['fields']
 
-    print((latest_record.Books))
-    print(type(latest_record.Books))
+    print(next(iter(latest_record.values())))
+    print(type(next(iter(latest_record.values())))
     return latest_record
 
 if __name__ == "__main__":
