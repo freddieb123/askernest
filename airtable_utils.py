e501b19a3e6db9983b5431541b20d73ccb028758
Fred Burgess <fredburgess@Freds-MacBook-Air-2.local>
2023-08-02 11:48:46 +0100

    forms2

--- airtable_utils.py
+++ airtable_utils.py
@@ -1,5 +1,5 @@
 # airtable_utils.py
-import requests
+#import requests
 import os
 from airtable import Airtable
 
@@ -10,34 +10,19 @@ AIRTABLE_BASE_ID = os.environ['BASE_KEY']
 AIRTABLE_TABLE_NAME = 'Draft2'
 
 def submit_form_data(name, age, location, interests):
-    # Airtable API endpoint
-    #url = f"https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}"
-    #headers = {
-        #"Authorization": f"Bearer {AIRTABLE_API_KEY}",
-        #"Content-Type": "application/json"
-    #}
-    payload = {
-        "records": [
-            {
-                "fields": {
-                    "Name": name,
-                    "Age": age,
-                    "Location": location,
-                    "Interests": interests
-                }
-            }
-        ]
-    }
+     # Initialize Airtable for the desired table
+    table = Airtable(AIRTABLE_BASE_ID, AIRTABLE_TABLE_NAME, api_key=AIRTABLE_API_KEY)
 
-    # Initialize Airtable for 'responses' table
-    responses_table = Airtable(base_key, 'Draft2', AIRTABLE_API_KEY)
-
-    # Insert books into 'responses' table
-    responses_table.insert({'Name': name})
-
+    # Insert the form data into the Airtable table
     try:
-        response = requests.post(url, headers=headers, json=payload)
-        response.raise_for_status()  # Check for any HTTP error
+        table.insert({
+            "Name": name,
+            "Age": age,
+            "Location": location,
+            "Interests": interests
+        })
+
         return True
-    except requests.exceptions.RequestException as e:
+    except Exception as e:
+        print("Error inserting data into Airtable:", str(e))
         return False
