1af2e16a219a4bea7d31b7026a5a8f3ae86c55c0
Fred Burgess <fredburgess@Freds-MacBook-Air-2.local>
2023-08-02 11:45:58 +0100

    forms2

--- airtable_utils.py
+++ airtable_utils.py
@@ -1,6 +1,8 @@
 # airtable_utils.py
 import requests
 import os
+from airtable import Airtable
+
 
 # Replace these with your actual Airtable API key and base ID
 AIRTABLE_API_KEY = os.environ['API_KEY']
@@ -9,11 +11,11 @@ AIRTABLE_TABLE_NAME = 'Draft2'
 
 def submit_form_data(name, age, location, interests):
     # Airtable API endpoint
-    url = f"https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}"
-    headers = {
-        "Authorization": f"Bearer {AIRTABLE_API_KEY}",
-        "Content-Type": "application/json"
-    }
+    #url = f"https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}"
+    #headers = {
+        #"Authorization": f"Bearer {AIRTABLE_API_KEY}",
+        #"Content-Type": "application/json"
+    #}
     payload = {
         "records": [
             {
@@ -26,6 +28,12 @@ def submit_form_data(name, age, location, interests):
             }
         ]
     }
+
+    # Initialize Airtable for 'responses' table
+    responses_table = Airtable(base_key, 'Draft2', AIRTABLE_API_KEY)
+
+    # Insert books into 'responses' table
+    responses_table.insert({'Name': name})
 
     try:
         response = requests.post(url, headers=headers, json=payload)
