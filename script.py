5689e71bd580a490a52ac83a4626b3ca3444406d
Fred Burgess <fredburgess@Freds-MacBook-Air-2.local>
2023-08-02 15:31:55 +0100

    forms2

--- script.py
+++ script.py
@@ -19,13 +19,14 @@ def main():
     latest_record = records[0]['fields']
 
     # construct the prompt
-    prompt = "My uncle lives in {}. And grew up in {}. He is {} years old. And his interests are {}. I would describe our relationship as {}. \n\nRecommend me 5 books based on this description that I could give him. Make sure the recommendations aren't really obvious. Please provide your recommendations as a Python dictionary, with the book title as the key and the author's name as the value. For example: {{\"To Kill a Mockingbird\": \"Harper Lee\", \"1984\": \"George Orwell\"}}. Remove any other text apart from the dictionary.".format(
+    prompt = "My uncle lives in {}. And grew up in {}. He is {} years old. And his interests are {}. And for context I would describe our relationship as {}. \n\nRecommend me 5 books based on this description that I could give him. Make sure the recommendations aren't really obvious. Please provide your recommendations as a Python dictionary, with the book title as the key and the author's name as the value. For example: {{\"To Kill a Mockingbird\": \"Harper Lee\", \"1984\": \"George Orwell\"}}. Remove any other text apart from the dictionary.".format(
     latest_record['Location'],
-    latest_record['Age'],
     latest_record['Grewup'],
+    latest_record['Age'],
     latest_record['Interests'],
     latest_record['Relationship']
     )
+    print(prompt)
     # Set your OpenAI key
     openai.api_key = os.environ['OPENAI_KEY']
 
