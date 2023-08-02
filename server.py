0a602cd9fc20e2ca98d9aa965e468edfe39e2ba1
Fred Burgess <fredburgess@Freds-MacBook-Air-2.local>
2023-08-02 14:04:04 +0100

    forms2

--- server.py
+++ server.py
@@ -14,6 +14,7 @@ logging.basicConfig(level=logging.INFO)
 def run_script():
     with app.app_context():
         g.books = script.main()
+        return g.books
 
 @app.route("/submitFormData", methods=["POST"])
 def handle_form_submission():
@@ -25,19 +26,28 @@ def handle_form_submission():
 
     if submit_form_data(name, age, location, interests):
         run_script()
-        return jsonify({"message": "Data inserted successfully"}), 200
+        return jsonify({"message": "Data inserted successfully", "books": g.books}), 200
     else:
         return jsonify({"message": "Error inserting data into Airtable"}), 500
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    # Check if book recommendations are available
-    #books = results.results()
-    #print(books)
-    #if books is None:
+    # Initialize books to None
+    books = None
+    print(books)
+    if books is None:
         return render_template('index.html')
-    #else:
-        #response = make_response(render_template('index.html', books=books), 200)
+    else:
+        # Redirect to the new page with books data as a query parameter
+        return redirect(url_for('display_books', books=books))
+
+@app.route('/display_books')
+def display_books():
+    # Retrieve the books data from the query parameter
+    books = request.args.get('books')
+
+    # Render the template with the books data
+    return render_template('books_table.html', books=books)
 
 
 if __name__ == '__main__':
--- static/app.js
+++ static/app.js
@@ -14,8 +14,12 @@ document.getElementById("userForm").addEventListener("submit", function (event) 
                 },
                 body: JSON.stringify(formData)
             })
-            .then(response => {
-                if (response.ok) {
+            .then(response => response.json())
+            .then(data => {
+                if (data.message === "Data inserted successfully") {
+                    // Update the page content dynamically with the new data
+                    console.log(data.books)
+                    updatePageContent(data.books);
                     alert("Form data submitted successfully!");
                 } else {
                     alert("Error submitting form data. Please try again later.");
@@ -26,3 +30,23 @@ document.getElementById("userForm").addEventListener("submit", function (event) 
                 alert("Error submitting form data. Please try again later.");
             });
         });
+
+        // Function to update the page content dynamically
+        function updatePageContent(books) {
+          // Get the table element
+          const table = document.getElementById("bookTable");
+
+          // Clear existing rows (except the header)
+          while (table.rows.length > 1) {
+            table.deleteRow(1);
+          }
+
+          // Create and append new rows for each book
+          for (const [title, author] of Object.entries(books)) {
+              const newRow = table.insertRow();
+              const titleCell = newRow.insertCell();
+              const authorCell = newRow.insertCell();
+              titleCell.innerText = title;
+              authorCell.innerText = author;
+              }
+          }
