724b9bee7e72f67fad7c53e55e562ff94686c4d9
Fred Burgess <fredburgess@Freds-MacBook-Air-2.local>
2023-08-02 10:01:13 +0100

    update

--- script.py
+++ script.py
@@ -19,7 +19,7 @@ def main():
     latest_record = records[0]['fields']
 
     # construct the prompt
-    prompt = "This description is about my {}: they grew up in {}. They describe themselves in 3 words as {}. Their main interests are {}. A typical meeting between us looks like {}. And I would describe the conversation as {}. \n\nFinally, they like {} books. \n\nRecommend me 5 books based on this description. Make sure the recommendations aren't really obvious. Please provide your recommendations as a Python dictionary, with the book title as the key and the author's name as the value. For example: {{\"To Kill a Mockingbird\": \"Harper Lee\", \"1984\": \"George Orwell\"}}.".format(
+    prompt = "This description is about my {}: they grew up in {}. They describe themselves in 3 words as {}. Their main interests are {}. A typical meeting between us looks like {}. And I would describe the conversation as {}. \n\nFinally, they like {} books. \n\nRecommend me 5 books based on this description. Make sure the recommendations aren't really obvious. Please provide your recommendations as a Python dictionary, with the book title as the key and the author's name as the value. For example: {{\"To Kill a Mockingbird\": \"Harper Lee\", \"1984\": \"George Orwell\"}}. Remove any other text apart from the dictionary.".format(
     latest_record['Relationship'],
     latest_record['Location'],
     latest_record['Three words'],
@@ -40,6 +40,7 @@ def main():
       ]
     )
     books = response.choices[0].message.content.strip()
+    print(books)
 
     # Initialize Airtable for 'Recommendations' table
     recommendations_table = Airtable(base_key, 'Recommendations', api_key)
